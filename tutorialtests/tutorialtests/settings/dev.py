c9dcdfd9f28c0a846ac54cbc6fa8c79c456b5088
vossisboss <vossmk@gmail.com>
2023-09-21 17:09:27 -0400

    Initial code commit.

---
+++ tutorialtests/tutorialtests/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7u3%3(kgb2pn!b$9a3&1*p_-#8wo@au$$4+d!esdhn-r-*00$h"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
